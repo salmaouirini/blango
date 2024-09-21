67512ebd7cbff569cec40dd5de24143f8c9ef3bc
salma ouirini <ouirinisalma1@gmail.com>
2024-09-21 16:28:28 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -45,9 +45,11 @@ class Dev(Configuration):
         "blog",
         "crispy_forms",
         "crispy_bootstrap5",
+        "debug_toolbar",
     ]
 
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -191,6 +193,7 @@ class Dev(Configuration):
       'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
       'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     ]
+    INTERNAL_IPS = ["192.168.11.179"]
 
 
 
