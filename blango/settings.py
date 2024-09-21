148743e98f9ad156d397876922761c73efa17e17
salma ouirini <ouirinisalma1@gmail.com>
2024-09-21 21:05:35 +0000

    Finish custom User model

--- blango/settings.py
+++ blango/settings.py
@@ -42,10 +42,12 @@ class Dev(Configuration):
         'django.contrib.sessions',
         'django.contrib.messages',
         'django.contrib.staticfiles',
+        "blango_auth",
         "blog",
         "crispy_forms",
         "crispy_bootstrap5",
         "debug_toolbar",
+        
     ]
 
     MIDDLEWARE = [
@@ -194,6 +196,7 @@ class Dev(Configuration):
       'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
     ]
     INTERNAL_IPS = ["192.168.11.179"]
+    AUTH_USER_MODEL = "blango_auth.User"
 
 
 
